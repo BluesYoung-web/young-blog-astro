7a8118f4273a1c8389c465e44b9f8fd32919aca3
BluesYoung-web <15171255945@163.com>
2022-09-04 11:28:47 +0800

    feat: :sparkles: node test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,28 @@
 /*
  * @Author: zhangyang
  * @Date: 2022-09-04 09:46:18
- * @LastEditTime: 2022-09-04 10:22:07
+ * @LastEditTime: 2022-09-04 10:40:27
  * @Description: 
  */
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import vercelAdapter from '@astrojs/vercel/serverless';
 
+import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vue from '@astrojs/vue';
+import unocss from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://young-blog-astro.vercel.app',
-	output: 'server',
-	adapter: vercelAdapter(),
-	integrations: [
+  site: 'https://young-blog-astro.vercel.app',
+  output: 'server',
+  adapter: vercelAdapter(),
+  integrations: [
 		mdx(),
 		sitemap({
-			customPages: [
-				'https://young-blog-astro.vercel.app',
-				'https://young-blog-astro.vercel.app/about',
-				'https://young-blog-astro.vercel.app/blog',
-			]
-		})
-	],
+			customPages: ['https://young-blog-astro.vercel.app', 'https://young-blog-astro.vercel.app/about', 'https://young-blog-astro.vercel.app/blog']
+		}),
+		vue(),
+		unocss()
+	]
 });
