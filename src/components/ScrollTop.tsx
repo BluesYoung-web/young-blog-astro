58abd0ead6ec3da6eec18fbeb3a99a8ce181092f
BluesYoung-web <15171255945@163.com>
2022-10-05 15:53:58 +0800

    feat: :sparkles: scroll to top component

---
+++ src/components/ScrollTop.tsx
@@ -0,0 +1,39 @@
+/*
+ * @Author: zhangyang
+ * @Date: 2022-10-05 15:51:07
+ * @LastEditTime: 2022-10-05 15:52:20
+ * @Description: 回到顶部
+ */
+import { defineComponent, h } from 'vue';
+import { useScroll } from '@vueuse/core';
+
+export default defineComponent({
+  props: {
+    show: { type: Number, default: 720 },
+  },
+  setup(props) {
+    const scorll = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    };
+    const { y } = useScroll(window);
+
+    return () => h(
+      <>
+        {
+          y.value > props.show
+            ? <div
+                className="fixed right-2 bottom-2 lg:right-10 lg:bottom-10 text-2xl text-gray-400 dark:text-purple-500 hover:cursor-pointer"
+                title="回到顶部"
+                onClick={() => scorll()}
+              >
+                <div className="i-bi-arrow-up-circle" />
+              </div>
+            : null
+        }
+      </>
+    );
+  }
+});
