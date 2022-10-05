bd764b6628c81a14151ab2a567ff90ff54a6c109
BluesYoung-web <15171255945@163.com>
2022-10-05 10:16:12 +0800

    chore: :hammer: change ssr to ssg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 /*
  * @Author: zhangyang
  * @Date: 2022-09-04 09:46:18
- * @LastEditTime: 2022-09-11 17:13:42
+ * @LastEditTime: 2022-10-05 10:13:32
  * @Description: 
  */
 import { defineConfig } from 'astro/config';
-import adapter from '@astrojs/netlify/functions';
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
@@ -15,8 +14,6 @@ import unocss from 'unocss/astro';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://young-blog-astro.vercel.app',
-  output: 'server',
-  adapter: adapter(),
   integrations: [
 		mdx(),
 		sitemap({
