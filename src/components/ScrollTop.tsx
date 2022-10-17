a31c502c9322341c3c4fa3cf7a356ad857ad297b
张扬 <bluesyoung_web@163.com>
2022-10-17 12:28:03 +0800

    feat: :sparkles: mobile add doc search

--- src/components/ScrollTop.tsx
+++ src/components/ScrollTop.tsx
@@ -1,17 +1,19 @@
 /*
  * @Author: zhangyang
  * @Date: 2022-10-05 15:51:07
- * @LastEditTime: 2022-10-10 09:26:13
+ * @LastEditTime: 2022-10-17 12:11:44
  * @Description: 回到顶部
  */
 import { defineComponent, h } from 'vue';
-import { isClient, useScroll } from '@vueuse/core';
+import { isClient, useScroll, useMediaQuery } from '@vueuse/core';
 
 export default defineComponent({
   props: {
     show: { type: Number, default: 720 },
   },
   setup(props) {
+    const isLargeScreen = useMediaQuery('(min-width: 1024px)');
+
     const scorll = () => {
       window.scrollTo({
         top: 0,
@@ -20,18 +22,40 @@ export default defineComponent({
     };
     const { y } = useScroll(isClient ? window : null);
 
+    const searchEvent = () => new CustomEvent('young:trigger-search');
+
+    const searchDoc = () => {
+      document.body.dispatchEvent(searchEvent());
+    };
     return () => h(
       <>
         {
           y.value > props.show
             ? <div
-                class="fixed z-100 right-2 bottom-2 lg:right-10 lg:bottom-10 text-2xl text-gray-400 dark:text-purple-500 hover:cursor-pointer"
-                title="回到顶部"
-                onClick={() => scorll()}
+                class="fixed z-100 right-2 bottom-2 lg:right-10 lg:bottom-10 text-2xl text-gray-400 dark:text-purple-500 hover:cursor-pointer flex flex-col"
               >
-                <div class="i-bi-arrow-up-circle" />
+                {
+                  !isLargeScreen.value && <div
+                    class="i-bi-search mb-2"
+                    title="搜索"
+                    onClick={() => searchDoc()}
+                  />
+                }
+                <div
+                  class="i-bi-arrow-up-circle"
+                  title="回到顶部"
+                  onClick={() => scorll()}  
+                />
               </div>
-            : null
+            : !isLargeScreen.value && <div
+                class="fixed z-100 right-2 bottom-2 lg:right-10 lg:bottom-10 text-2xl text-gray-400 dark:text-purple-500 hover:cursor-pointer flex flex-col"
+              >
+                <div
+                  class="i-bi-search mb-2"
+                  title="搜索"
+                  onClick={() => searchDoc()}
+                />
+              </div>
         }
       </>
     );
---
+++ src/components/SearchPopup.vue
@@ -0,0 +1,43 @@
+<!--
+ * @Author: zhangyang
+ * @Date: 2022-10-17 11:27:59
+ * @LastEditTime: 2022-10-17 12:24:22
+ * @Description: 
+-->
+<script lang="ts" setup>
+import 'uno.css';
+import { NDialog } from 'naive-ui';
+import MyDoc from '@/components/MyDoc.vue';
+import type { DocTree } from '@/utils/generateDocTree';
+
+type Props = {
+  total: number;
+  tree: DocTree[];
+};
+defineProps<Props>();
+
+const hideSearchEvent = () => new CustomEvent('young:hide-search');
+
+const hide = () => {
+  document.body.dispatchEvent(hideSearchEvent());
+};
+</script>
+<template>
+  <div id="search-popup" class="search hidden">
+    <NDialog
+      title="文章搜索"
+      mask-closable
+      type="info"
+      class="w-100vw h-100vh"
+      @close="hide"
+    >
+      <MyDoc :total="total" :tree="tree" />
+    </NDialog>
+  </div>
+</template>
+
+<style lang="scss">
+.search {
+  @apply fixed left-0 top-0 z-999;
+}
+</style>
